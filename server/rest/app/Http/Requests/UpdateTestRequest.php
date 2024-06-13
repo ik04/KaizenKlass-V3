c94b0e6f373ea47f237c0cb1cbc1dbe1882905e6
ishaan khurana <ishaankhurana123@gmail.com>
2024-06-13 17:09:21 +0530

    feat: shift api endpoints from v3 to current repo

---
+++ server/rest/app/Http/Requests/UpdateTestRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTestRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "title" => "string|nullable",
+            "subject_uuid" =>"string|nullable",
+            "exam_date" =>"nullable|date_format:Y-m-d|after_or_equal:today",
+        ];
+    }
+}
--- server/rest/routes/api.php
+++ server/rest/routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\ResourceController;
 use App\Http\Controllers\SelectedSubjectController;
 use App\Http\Controllers\SolutionController;
 use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\TestResourceController;
 use App\Http\Controllers\UserController;
 use App\Models\Subject;
 use Carbon\Carbon;
@@ -82,14 +84,38 @@ Route::prefix("v2")->group(function(){
         Route::prefix("get")->group(function(){
             Route::get("selected-subjects",[SelectedSubjectController::class,"getSelectedSubjects"]);
             Route::get("selected-subjects/assignments",[AssignmentController::class,"getAssignmentsWithSelectedSubjects"]);
+
+            Route::get("subjects/search/{query}",[SubjectController::class,"searchSubjects"]);
+            Route::get("selected-subjects/search/{query}",[SelectedSubjectController::class,"searchSelectedSubjects"]);
+
+            Route::get("selected-subjects/tests",[TestController::class,"getTestsWithSelectedSubjects"]);
+            Route::get("subjects/{uuid}/tests",[TestController::class,"getTestsBySubject"]);
+
+            Route::get("test/{uuid}",[TestController::class,"getTest"]);
         });
         Route::prefix("add")->group(function(){
             Route::post("selected-subjects",[SelectedSubjectController::class,"selectSubjects"]);
             Route::post("selected-subject",[SelectedSubjectController::class,"selectSubject"]);
         });
+        Route::prefix("update")->group(function(){
+            Route::put("test-resource/{uuid}",[TestResourceController::class,"updateOwnTestResource"]); 
+        });
         Route::prefix("delete")->group(function(){
             Route::post("selected-subject",[SelectedSubjectController::class,"removeSelectedSubject"]);
             Route::post("selected-subjects",[SelectedSubjectController::class,"removeAllSelectedSubject"]);
+        });
+    });
+    Route::middleware(["auth:sanctum","checkCrosschecker"])->group(function(){
+        Route::prefix("add")->group(function(){
+            Route::post("test",[TestController::class,"createTest"]);
+        });
+        Route::prefix("get")->group(function(){
+        });
+        Route::prefix("remove")->group(function(){
+            Route::delete("test/{uuid}",[TestController::class,"deleteTest"]); 
+        });
+        Route::prefix("update")->group(function(){
+            Route::put("test/{uuid}",[TestController::class,"updateTest"]); 
         });
     });
 });
