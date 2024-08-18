5850a125d7b07d950882155727859a9875bdc88f
ishaan khurana <ishaankhurana123@gmail.com>
2024-08-19 04:38:00 +0530

    feat: order selected subjects by name

--- server/rest/app/Services/SelectedSubjectService.php
+++ server/rest/app/Services/SelectedSubjectService.php
@@ -26,7 +26,7 @@ class SelectedSubjectService{
         }
      }
     public function getSelectedSubjects($userId){
-        $selectedSubjects = SelectedSubject::join("subjects","selected_subjects.subject_id","=","subjects.id")->select("subjects.subject_uuid","subjects.subject")->where("selected_subjects.user_id",$userId)->paginate(8);
+        $selectedSubjects = SelectedSubject::join("subjects","selected_subjects.subject_id","=","subjects.id")->select("subjects.subject_uuid","subjects.subject")->where("selected_subjects.user_id",$userId)->orderBy("subjects.subject")->paginate(8);
         return $selectedSubjects;
     }
     public function getAllSelectedSubjects($userId){
