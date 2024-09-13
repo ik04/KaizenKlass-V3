c6578625386d037d94adc0d102032f55808f36f9
ik04 <ishaankhurana123@gmail.com>
2024-09-14 00:34:31 +0530

    feat: init announcement categories and announcements

---
+++ server/rest/database/seeders/AnnouncementCategorySeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AnnouncementCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $relativePath = __DIR__ . "/init/announcement_categories.json";
+
+    }
+}
